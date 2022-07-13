34b9931287fab8caedf3f57024a9fbfa199020eb
Hettore Eduardo <duh.barretoo@gmail.com>
2022-07-13 18:16:00 -0300

    Adicionando operções de update e remove.

--- application/Program.java
+++ application/Program.java
@@ -9,24 +9,36 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
+		Locale.setDefault(Locale.US); //Para resolver a questão do uso do .
+		Scanner sc = new Scanner(System.in); //Para conseguir ler as informações.Lembrar de fechar sc.close()
 		
-		Product product = new Product();
-		System.out.println("Enter product data: ");
+		Product product = new Product(); //Não esqueça de importar a classe.
+		System.out.println("Enter product data: "); //
 		System.out.print("Name:");
-		product.name = sc.nextLine();
+		product.name = sc.nextLine(); //Leitura do nome do produto e ja armazena na variável name
 		System.out.print("Price: ");
-		product.price = sc.nextDouble();
+		product.price = sc.nextDouble(); //Leitura do valor e armazena na variável price
 		System.out.print("Quantity in stock: ");
-		product.quantity = sc.nextInt();
+		product.quantity = sc.nextInt(); // Leitura da quantidade e armazena na variável quantity
 		
+		System.out.println();
+		System.out.println("Product data: " + product);
 		
+		System.out.println();
+		System.out.println("Enter the number of products to be added in stock: ");
+		int quantity = sc.nextInt();
+		product.addProducts(quantity);
 		
-		System.out.println(product);
+		System.out.println();
+		System.out.println("Updated data: " + product);
 		
+		System.out.println();
+		System.out.println("Enter the number of products to be removed from stock: ");
+		quantity = sc.nextInt();
+		product.removeProducts(quantity);
 		
-		
+		System.out.println();
+		System.out.println("Updated data: " + product);
 		
 		sc.close();
 		
--- entities/Product.java
+++ entities/Product.java
@@ -1,23 +1,29 @@
 package entities;
 
 public class Product {
-
+	//Definindo os atributos da classe Product
 	public String name;
 	public double price;
 	public int quantity;
 	
+	//Definindo os métodos
+	//operação para devolver o valor total do estoque
 	public double totalValueInStock() {
 		return price * quantity;
 	}
 	
+	//Operação para adicionar produtos ao estoque.
+	//Usamos o this para referenciar o atributo da classe e não o parametro da função.
 	public void addProducts(int quantity) {
 		this.quantity += quantity;
 	}
 	
+	//Operação para remover produto do estoque.
 	public void removeProducts(int quantity) {
 		this.quantity -= quantity;
 	}
 	
+	//Operação para converter o objeto para string, sobreposição 
 	public String toString() {
 		return name
 				+ ", $"
